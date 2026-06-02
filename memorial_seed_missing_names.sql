a84d29818628e591b4431e3412926cdfea6d74cb
Tito <pinjur88@gmail.com>
2026-06-02 10:12:43 -0400

    db: add missing surnames to memorial seed script

---
+++ memorial_seed_missing_names.sql
@@ -0,0 +1,180 @@
+/** MEMORIAL DATA SEED: MISSING NAMES FROM IMAGE */
+
+INSERT INTO clans (
+    surname, 
+    prevalence_hr, world_distribution_hr, description_hr,
+    prevalence_en, world_distribution_en, description_en,
+    prevalence_de, world_distribution_de, description_de
+) VALUES 
+('Arambašić', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ARAMBAŠIĆ (ogranak Beševića) – Nikoljdan
+- 1756. арамбаша Стојан Бешевић (вјероватно предак); у 19. вијеку Тодор Арамбашић; 1948. године пописано је 13 Арамбашића у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Arambašić (branch of Bešević) - St. Nicholas
+- 1756 harambaša Stojan Bešević (probable ancestor); in the 19th century Todor Arambašić; in 1948, 13 Arambašićs were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Arambašić (Zweig von Bešević) - St. Nikolaus
+- 1756 harambaša Stojan Bešević (wahrscheinlicher Vorfahr); im 19. Jahrhundert Todor Arambašić; 1948 wurden 13 Arambašićs in Baljci verzeichnet.'),
+
+('Bajić', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+BAJIĆ (BAIĆ) (u 18. i 19. вијеку)
+- 1740-их Јован Баић; ишчезли из села у 20. вијеку.',
+'Data is being collected.', 'Data is being collected.', 'Bajić (Baić) (in 18th and 19th centuries)
+- 1740s Jovan Baić; disappeared from the village in the 20th century.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Bajić (Baić) (im 18. und 19. Jahrhundert)
+- 1740er Jovan Baić; im 20. Jahrhundert aus dem Dorf verschwunden.'),
+
+('Borojević', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+BOROJEVIĆ (огранак Бешевића из 18. и 19. вијека) – Никољдан
+- 1711. Бороје Бешевић; 1756. Петар Бешевић-Боројевић.',
+'Data is being collected.', 'Data is being collected.', 'Borojević (branch of Bešević from the 18th and 19th centuries) - St. Nicholas
+- 1711 Boroje Bešević; 1756 Petar Bešević-Borojević.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Borojević (Zweig von Bešević aus dem 18. und 19. Jahrhundert) - St. Nikolaus
+- 1711 Boroje Bešević; 1756 Petar Bešević-Borojević.'),
+
+('Drljanac', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+DRLJANAC (Дрљан - надимак за Бибић) – Никољдан
+- Бибићи су у 19. вијеку носили надимке Лошета, Дрљан и Амиџа. 1948. године пописана су 54 Бибића у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Drljanac (Drljan - nickname for Bibić) - St. Nicholas
+- The Bibić family in the 19th century carried nicknames Lošeta, Drljan, and Amidža. In 1948, 54 Bibićs were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Drljanac (Drljan - Spitzname für Bibić) - St. Nikolaus
+- Die Familie Bibić trug im 19. Jahrhundert die Spitznamen Lošeta, Drljan und Amidža. 1948 wurden 54 Bibićs in Baljci verzeichnet.'),
+
+('Gligorić', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ГЛИГОРИЋ – Ђурђевдан
+- око 1750. Јован Глигорић, а рођени Алекса и Крстан Глигорић; 1948. године пописана су 3 Глигорића у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Gligorić - St. George
+- Around 1750 Jovan Gligorić, and born Aleksa and Krstan Gligorić; in 1948, 3 Gligorićs were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Gligorić - St. Georg
+- Um 1750 Jovan Gligorić und geboren Aleksa und Krstan Gligorić; 1948 wurden 3 Gligorićs in Baljci verzeichnet.'),
+
+('Gugić', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ГУГИЋ (Римокатолици)
+- 1756. Иван Гугић (вјероватно доселили као ковачи/Цингано); 1948. године пописано је 14 Гугића у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Gugić (Roman Catholics)
+- 1756 Ivan Gugić (likely migrated as blacksmiths/Cingano); in 1948, 14 Gugićs were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Gugić (Römisch-katholisch)
+- 1756 Ivan Gugić (wahrscheinlich als Schmiede/Cingano eingewandert); 1948 wurden 14 Gugićs in Baljci verzeichnet.'),
+
+('Gutić', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ГУТИЋ – Ђурђевдан
+- око 1750. рођен Јован Гутић; 1948. године пописано је 7 Гутића у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Gutić - St. George
+- Around 1750 born Jovan Gutić; in 1948, 7 Gutićs were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Gutić - St. Georg
+- Um 1750 geboren Jovan Gutić; 1948 wurden 7 Gutićs in Baljci verzeichnet.'),
+
+('Jajčanin', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ЈАЈЧАНИН (Римокатолици у 18. и 19. вијеку)
+- 1756. Мартин и Петар Јајчанин; 1835. Лука Јајчанин (у матичним књигама записивани као ковачи/Цингано).',
+'Data is being collected.', 'Data is being collected.', 'Jajčanin (Roman Catholics in 18th and 19th centuries)
+- 1756 Martin and Petar Jajčanin; 1835 Luka Jajčanin (recorded in registries as blacksmiths/Cingano).',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Jajčanin (Römisch-katholisch im 18. und 19. Jahrhundert)
+- 1756 Martin und Petar Jajčanin; 1835 Luka Jajčanin (in Registern als Schmiede/Cingano verzeichnet).'),
+
+('Kaliger', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', 'Kaliger (Podaci o porijeklu se još prikupljaju i nadopunjuju).',
+'Data is being collected.', 'Data is being collected.', 'Kaliger (Origin data is currently being collected and updated).',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Kaliger (Herkunftsdaten werden derzeit gesammelt und aktualisiert).'),
+
+('Knez', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+КНЕЗ (постојали у 18. и 19. вијеку)
+- око 1740. рођен Јаков Кнез; иселили у Славонију/Срем средином 19. вијека.',
+'Data is being collected.', 'Data is being collected.', 'Knez (existed in 18th and 19th centuries)
+- Around 1740 born Jakov Knez; emigrated to Slavonia/Srem in the mid 19th century.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Knez (existierten im 18. und 19. Jahrhundert)
+- Um 1740 geboren Jakov Knez; wanderten Mitte des 19. Jahrhunderts nach Slawonien/Syrmien aus.'),
+
+('Maksić', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+МАКСИЋ (огранак Бибића) – Никољдан
+- 1730-их рођен Марко Бибић-Максић; ишчезли из села у 20. вијеку.',
+'Data is being collected.', 'Data is being collected.', 'Maksić (branch of Bibić) - St. Nicholas
+- 1730s born Marko Bibić-Maksić; disappeared from the village in the 20th century.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Maksić (Zweig von Bibić) - St. Nikolaus
+- 1730er geboren Marko Bibić-Maksić; im 20. Jahrhundert aus dem Dorf verschwunden.'),
+
+('Mudrić', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+МУДРИЋ (раније Богдановић)
+- 1709. Илија Богдановић из Баљака... презиме се наводи и као Богдановић-Мудре; крајем 18. вијека Марко Мудрић; 1948. године пописан је 1 Мудрић у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Mudrić (formerly Bogdanović)
+- 1709 Ilija Bogdanović from Baljci... surname is also listed as Bogdanović-Mudre; late 18th century Marko Mudrić; in 1948, 1 Mudrić was recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Mudrić (früher Bogdanović)
+- 1709 Ilija Bogdanović aus Baljci... Nachname wird auch als Bogdanović-Mudre aufgeführt; spätes 18. Jahrhundert Marko Mudrić; 1948 wurde 1 Mudrić in Baljci verzeichnet.'),
+
+('Pudjaš', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ПУЂАШ (у 18. и 19. вијеку)
+- 1756. Марко Пуђаш; иселили у Мирковце код Винковаца.',
+'Data is being collected.', 'Data is being collected.', 'Puđaš (in 18th and 19th centuries)
+- 1756 Marko Puđaš; emigrated to Mirkovci near Vinkovci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Puđaš (im 18. und 19. Jahrhundert)
+- 1756 Marko Puđaš; nach Mirkovci bei Vinkovci ausgewandert.'),
+
+('Šimac', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', 'Šimac (Podaci o porijeklu se još prikupljaju i nadopunjuju).',
+'Data is being collected.', 'Data is being collected.', 'Šimac (Origin data is currently being collected and updated).',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Šimac (Herkunftsdaten werden derzeit gesammelt und aktualisiert).'),
+
+('Špar', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', 'Špar / Sparovac (Podaci o porijeklu se još prikupljaju i nadopunjuju).',
+'Data is being collected.', 'Data is being collected.', 'Špar / Sparovac (Origin data is currently being collected and updated).',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Špar / Sparovac (Herkunftsdaten werden derzeit gesammelt und aktualisiert).'),
+
+('Stole', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', 'Stole / Stolinice (Podaci o porijeklu se još prikupljaju i nadopunjuju).',
+'Data is being collected.', 'Data is being collected.', 'Stole / Stolinice (Origin data is currently being collected and updated).',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Stole / Stolinice (Herkunftsdaten werden derzeit gesammelt und aktualisiert).'),
+
+('Tarlać', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ТАРЛАЋ (огранак Бешевића) – Никољдан
+- 1756. Иван/Јован Бешевић-Тарлаћ; 1948. године пописано је 13 Тарлаћа у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Tarlać (branch of Bešević) - St. Nicholas
+- 1756 Ivan/Jovan Bešević-Tarlać; in 1948, 13 Tarlaćs were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Tarlać (Zweig von Bešević) - St. Nikolaus
+- 1756 Ivan/Jovan Bešević-Tarlać; 1948 wurden 13 Tarlaćs in Baljci verzeichnet.'),
+
+('Tetek', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ТЕТЕК (вјероватно огранак Бибића) – Никољдан
+- 1766. Лазо Тетек; 1948. године пописано је 5 Тетека у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Tetek (likely branch of Bibić) - St. Nicholas
+- 1766 Lazo Tetek; in 1948, 5 Teteks were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Tetek (wahrscheinlich Zweig von Bibić) - St. Nikolaus
+- 1766 Lazo Tetek; 1948 wurden 5 Teteks in Baljci verzeichnet.'),
+
+('Tomasović', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', 'Tomasović (Podaci o porijeklu se još prikupljaju i nadopunjuju).',
+'Data is being collected.', 'Data is being collected.', 'Tomasović (Origin data is currently being collected and updated).',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Tomasović (Herkunftsdaten werden derzeit gesammelt und aktualisiert).'),
+
+('Tomović', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', 'Tomović (Podaci o porijeklu se još prikupljaju i nadopunjuju).',
+'Data is being collected.', 'Data is being collected.', 'Tomović (Origin data is currently being collected and updated).',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Tomović (Herkunftsdaten werden derzeit gesammelt und aktualisiert).'),
+
+('Tošić', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ТОШИЋ – Ђурђевдан
+- око 1760. рођен Стојан Тошић; 1948. године пописано је 8 Тошића у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Tošić - St. George
+- Around 1760 born Stojan Tošić; in 1948, 8 Tošićs were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Tošić - St. Georg
+- Um 1760 geboren Stojan Tošić; 1948 wurden 8 Tošićs in Baljci verzeichnet.'),
+
+('Vidović', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ВИДОВИЋ (Римокатолици)
+- дошли из сусједног села Градац крајем 19. вијека; 1948. године пописано је 14 Видовића у Баљцима.',
+'Data is being collected.', 'Data is being collected.', 'Vidović (Roman Catholics)
+- arrived from the neighboring village Gradac at the end of the 19th century; in 1948, 14 Vidovićs were recorded in Baljci.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Vidović (Römisch-katholisch)
+- kamen Ende des 19. Jahrhunderts aus dem Nachbardorf Gradac; 1948 wurden 14 Vidovićs in Baljci verzeichnet.'),
+
+('Amidža', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+АМИДЖА (надимак за Бибић) – Никољдан
+- Бибићи су у 19. вијеку носили надимке Лошета, Дрљан и Амиџа.',
+'Data is being collected.', 'Data is being collected.', 'Amidža (nickname for Bibić) - St. Nicholas
+- The Bibić family in the 19th century carried nicknames Lošeta, Drljan, and Amidža.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Amidža (Spitzname für Bibić) - St. Nikolaus
+- Die Familie Bibić trug im 19. Jahrhundert die Spitznamen Lošeta, Drljan und Amidža.'),
+
+('Lošeta', 'Podaci se prikupljaju.', 'Podaci se prikupljaju.', '[IZVOR: POREKLO.RS - BALJCI]
+ЛОШЕТА (надимак за Бибић) – Никољдан
+- Бибићи су у 19. вијеку носили надимке Лошета, Дрљан и Амиџа.',
+'Data is being collected.', 'Data is being collected.', 'Lošeta (nickname for Bibić) - St. Nicholas
+- The Bibić family in the 19th century carried nicknames Lošeta, Drljan, and Amidža.',
+'Daten werden gesammelt.', 'Daten werden gesammelt.', 'Lošeta (Spitzname für Bibić) - St. Nikolaus
+- Die Familie Bibić trug im 19. Jahrhundert die Spitznamen Lošeta, Drljan und Amidža.')
+
+ON CONFLICT (surname) DO UPDATE SET
+    description_hr = EXCLUDED.description_hr,
+    description_en = EXCLUDED.description_en,
+    description_de = EXCLUDED.description_de;
